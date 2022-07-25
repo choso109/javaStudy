3b7b289275416486a795d13a8251d8eb6518fead
301-19 <301-19@DESKTOP-G641E80>
2022-07-25 15:52:56 +0900

    자바 처음에

---
+++ Chap011/src/Hello.java
@@ -0,0 +1,12 @@
+// 한줄 주석 : 프로그래머에게 남기는 노트
+// public, class, static, void : 키워드(예약어)
+// main 메소드 이름 : 클래스를 실행하면 첫번재 문장 부터 실행 / 프로젝트 안에서 일반적으로 메인 메소드는 하나
+// args : 변수명, 프로그래머가 이름 짓는것, 사용자 정의어
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕 Java!!");
+	}
+
+}
---
+++ Chap011/src/sec06/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec06.exam01;
+/*
+ * 프로그램의 설명, 수정 이력
+ * 여러줄주석
+ * ㅇㅇㅇㅇㅇㅇㅇㅇ
+ */
+
+public class Hello {
+	public static void main(String args []) {
+		System.out.println("반갑습니다. 여러분");
+	}
+}
